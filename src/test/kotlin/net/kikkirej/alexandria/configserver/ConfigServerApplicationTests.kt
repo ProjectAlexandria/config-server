a30dd1ea29be60f299033a11101fa07561ca54b4
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-15 11:23:20 +0100

    🎉

---
+++ src/test/kotlin/net/kikkirej/alexandria/configserver/ConfigServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.kikkirej.alexandria.configserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
